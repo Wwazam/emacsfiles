fca96c88d01a775a50f7470948771779d64ce586
Wwazam <basile.pracca@gmail.com>
2017-05-24 17:54:43 +0200

    clean install key-chord, clean init.el file

--- elpa/rebecca-theme-20170406.2136/rebecca-theme-autoloads.el
+++ elpa/rebecca-theme-20170406.2136/rebecca-theme-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "rebecca-theme" "rebecca-theme.el" (22820 12078
+;;;### (autoloads nil "rebecca-theme" "rebecca-theme.el" (22821 20485
 ;;;;;;  0 0))
 ;;; Generated autoloads from rebecca-theme.el
 
