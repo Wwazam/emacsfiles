f2f9aaa86c260185687007ea32d8844b633353d2
Wwazam <basile.pracca@gmail.com>
2017-06-23 10:05:18 +0200

    HUGE update / lot of modes / commits from maggit didn't reach

---
+++ elpa/minesweeper-20150413.2222/minesweeper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "minesweeper" "20150413.2222" "play minesweeper in Emacs" 'nil :url "https://bitbucket.org/zck/minesweeper.el" :keywords '("game" "fun" "minesweeper" "inane" "diversion"))
