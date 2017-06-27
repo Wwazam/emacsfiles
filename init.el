aa6659512a2b1a7630703bb7f997703e46c76a06
Wwazam <basile.pracca@gmail.com>
2017-06-27 18:24:34 +0200

    config scrivener

--- init.el
+++ init.el
@@ -34,9 +34,17 @@ re-downloaded in order to locate PACKAGE."
    (quote
     ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3eb8dec7c15ce9eb3584ec9846175d963f838eaeccf2490f8ab3daff59862212" "9541f1dc11258239ef02aa1a5e9db3e1e46bc8fb1d7dbe83946c1541ae6dbdf9" default)))
  '(inhibit-startup-screen t)
+ '(org-capture-templates
+   (quote
+    (("j" "Journal entry" entry
+      (file "~/org/notes.org")
+      "\\t%U %^{Title}\\n%?"))))
  '(package-selected-packages
    (quote
-    (ace-jump-mode org-bullets zenburn-theme monokai-theme sentence-highlight org-journal rainbow-delimiters magit minesweeper evil-leader helm windsize autopair elpy importmagic jedi key-chord inkpot-theme rebecca-theme toxi-theme evil))))
+    (buffer-move 0blayout visual-fill-column auctex ace-jump-mode org-bullets zenburn-theme monokai-theme sentence-highlight org-journal rainbow-delimiters magit minesweeper evil-leader helm windsize autopair elpy importmagic jedi key-chord inkpot-theme rebecca-theme toxi-theme evil)))
+ '(preview-gs-options
+   (quote
+    ("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -58,6 +66,23 @@ re-downloaded in order to locate PACKAGE."
 
 (require 'ace-jump-mode)
 (define-key evil-normal-state-map (kbd "C-.") 'ace-jump-mode)
+
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;###################################################################################################
+;#    ## ### #     #     ##  ##  #     # ### #     #################################################
+;# ### # ### # ##### ######      # ### # ### # #####################################################
+;# #  ## ### #   ###   #### #  # # ### # ### #   ###################################################
+;# ### # ### # ##### ###### #### # ### ## # ## #####################################################
+;#    ##     # ##### ###### #### #     ### ###     #################################################
+;###################################################################################################
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+(require 'buffer-move)
+(global-set-key (kbd "<C-S-up>")     'buf-move-up)
+(global-set-key (kbd "<C-S-down>")   'buf-move-down)
+(global-set-key (kbd "<C-S-left>")   'buf-move-left)
+(global-set-key (kbd "<C-S-right>")  'buf-move-right)
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;###################################################################################################
@@ -103,6 +128,16 @@ re-downloaded in order to locate PACKAGE."
 
 (require 'sentence-highlight)
 (add-hook 'org-mode-hook 'auto-fill-mode)
+
+;; Changes all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Turn off the menu bar at the top of each frame because it's distracting
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+;; Hide scroll bars
+(scroll-bar-mode -1)
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;###################################################################################################
@@ -162,6 +197,68 @@ re-downloaded in order to locate PACKAGE."
 
 ;; hide the // or ** or +--+ or __ when stylizing 
 (setq org-hide-emphasis-markers t)
+
+;; auto indent text in headings
+(setq org-startup-indented 1)
+
+;; setup interpreters for source code blocks
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)))
+
+;; org capture
+(define-key global-map "\C-cc" 'org-capture)
+(setq org-default-notes-file "c:\\Users\\bpracca\\Documents\\_Notes\\capture\\note.org")
+
+
+
+
+;; An override for this function to make inserting headings work
+;; a bit better. Makes O respect heading content.
+(defun shellhead/smart-org-insert ()
+  "Creates a new heading if currently in a heading, creates a new list item 
+   if in a list, or creates a newline if neither."
+  (interactive)
+  (cond
+   ((org-at-heading-p) (org-insert-heading-respect-content) (evil-insert-state))
+   ((org-at-item-p) (org-insert-item))))
+
+(defun evil-org-eol-call (fun)
+    "Go to end of line and call provided function.
+  FUN function callback"
+    (end-of-visible-line)
+    (funcall fun)
+    (evil-append nil)
+    )
+
+(defun org-tree-open-in-right-frame ()
+  (interactive)
+  (org-tree-to-indirect-buffer)
+  (windmove-right))
+
+(add-hook 'org-mode-hook 
+          (lambda ()
+            ;; TODO: set fringe/gutter mode and theme by mode, no fringe and white them for ORG files
+            ;; fringe and black theme for code
+            (fringe-mode 0)
+            
+            ;; (set-frame-parameter (window-frame) 'background-mode 'dark)
+            ;; (enable-theme 'leuven)
+
+            (visual-fill-column-mode)
+            (define-key evil-normal-state-local-map [S-return] (quote org-tree-open-in-right-frame))
+            (define-key evil-normal-state-local-map [return] (quote org-tree-to-indirect-buffer))
+))
+
+(define-key global-map (kbd "C-c C-h")
+  (lambda ()
+    (interactive)
+    (delete-other-windows)
+    (split-window-right)
+    (enlarge-window-horizontally -9999)
+    (enlarge-window-horizontally 30)
+  ))
+
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;###################################################################################################
@@ -179,12 +276,28 @@ re-downloaded in order to locate PACKAGE."
 (setq key-chord-two-keys-delay 1)
 (key-chord-mode 1)
 
-(require 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-leader "<SPC>")
-
 ;;SHORTCUTS
 ;;;; Esc on jj
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 (key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
 (evil-leader/set-key "/" 'evil-ex-nohighlight)
+
+(require 'evil-leader)
+(evil-leader/set-leader "<SPC>")
+(global-evil-leader-mode)
+(evil-leader/set-key
+  "f" 'helm-find-files
+  "b" 'helm-mini
+  "-" 'text-scale-decrease
+  "+" 'text-scale-increase
+  "x" 'helm-M-x
+  "r" 'helm-M-x
+  "k" 'kill-buffer)
+(evil-leader/set-key-for-mode 'org-mode
+  "t"  'org-show-todo-tree
+  "a"  'org-agenda
+  "c"  'org-archive-subtree
+  "l"  'evil-org-open-links
+  "o"  'org-open-at-point
+  "e"  'outline-show-branches
+)
